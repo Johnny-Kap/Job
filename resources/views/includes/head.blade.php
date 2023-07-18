8b815f4168981b0db95b9c46f63663030f99d95e
Johnny-Kap <johnnyfokou@gmail.com>
2023-07-18 19:43:29 +0100

    Update 18/07

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+
+<head>
     <meta charset="utf-8">
-    <meta name="keywords" content="HTML5 Template" />
-    <meta name="description" content="Jobber - Job Board HTML5 Template" />
+    <meta name="keywords" content="Portneufms" />
+    <meta name="description" content="Portneufms, Déposez votre CV et obtenez l'emploi souhaité" />
     <meta name="author" content="potenzaglobalsolutions.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>PMS</title>
@@ -13,5 +14,3 @@
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700" rel="stylesheet">
-
-
